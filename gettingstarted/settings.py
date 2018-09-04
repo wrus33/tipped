60280c84a92655cfe8426dbfe726c92cbac8ddac
Willy Russell <wrus33@gmail.com>
2018-09-03 23:02:39 -0500

    deployment testing

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -85,9 +85,7 @@ DATABASES = {
     }
 }
 
-AUTHENTICATION_BACKENDS = (
-                           ('django.contrib.auth.backends.ModelBackend'),
-)
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
