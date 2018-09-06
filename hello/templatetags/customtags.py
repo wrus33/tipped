a172d6b8a6022d252d624bca4ba1a141a8fbdab9
Willy Russell <wrus33@gmail.com>
2018-09-05 19:46:43 -0500

    added more template tags for statistics

--- hello/templatetags/customtags.py
+++ hello/templatetags/customtags.py
@@ -1,11 +1,20 @@
 from django import template
-from django.db.models import Avg
-from ..models import Shift
+from django.db.models import Avg, Sum
+from hello.models import Shift
 
 
 register = template.Library()
 
 @register.simple_tag
+def count():
+	return Shift.objects.count()
+
+@register.simple_tag
 def avg():
-	average = Shift.objects.aggregate(Avg('amount'))
-	return average
+	return Shift.objects.aggregate(Avg('amount'))
+
+@register.simple_tag
+def total():
+	return Shift.objects.aggregate(Sum('amount'))
+
+
